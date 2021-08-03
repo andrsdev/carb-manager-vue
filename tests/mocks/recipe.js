6bb70b39196f7b208bb37d32ee49d35557a4a40c
Andres Sanabria <andrs.dev@gmail.com>
2021-08-02 21:53:23 -0600

    add some basic tests

---
+++ tests/mocks/recipe.js
@@ -0,0 +1,27 @@
+const recipe = {
+  id: "1",
+  isPremium: true,
+  title:
+    "Low Carb Buffalo Chicken Mac And Cheese W Smashed Cauliflower Medallions",
+  image:
+    "https://images.carbmanager.com/iYKrSEf7P6EAGx3desxGmcPJTVea2lJoBiXom24tevA/resize:fit:535/L2Fzc2V0cy5jYXJibWFuYWdlci5jb20vby91c2VyJTJGNGdkMlJoRVpqM2NGR1NKWDVuYjhFQzROWjBEMiUyRmltYWdlcyUyRmJiNmExNjY4LTU1NDMtNGE3Ny1hMDQ0LWQ0ODVhM2U0ZDMwNS5qcGc_YWx0PW1lZGlh",
+  rating: {
+    count: 200,
+    score: 4.5,
+  },
+  units: {
+    proteins: "g",
+    carbs: "g",
+    fats: "g",
+    energy: "kJ",
+  },
+  nutrients: {
+    proteins: 20,
+    carbs: 16,
+    fats: 6,
+  },
+  energy: 774,
+  preparationTimeMinutes: 75,
+};
+
+module.exports = { recipe };
--- tests/unit/PremiumRecipeCard.spec.js
+++ tests/unit/PremiumRecipeCard.spec.js
@@ -1,3 +1,41 @@
+import { shallowMount } from "@vue/test-utils";
+import flushPromises from "flush-promises";
+const fetchMock = require("jest-fetch-mock");
+const { recipe } = require("../mocks/recipe");
+import PremiumRecipeCard from "../../src/components/PremiumRecipeCard.vue";
+
+fetchMock.enableFetchMocks();
+
 describe("PremiumRecipeCard.vue", () => {
-  /* Tests go here */
+  fetch.mockResponse(JSON.stringify(recipe));
+  const wrapper = shallowMount(PremiumRecipeCard);
+
+  it("renders correctly", async () => {
+    await flushPromises();
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it("fetch recipe data", async () => {
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(
+      "https://60c10ba2b8d367001755666e.mockapi.io/recipe/1"
+    );
+  });
+
+  it("renders title", async () => {
+    await flushPromises();
+    expect(wrapper.text()).toContain(recipe.title);
+  });
+
+  it("shows Calories when energyUnits is 'calories'", async () => {
+    wrapper.setProps({ energyUnits: "calories" });
+    await flushPromises();
+    expect(wrapper.text()).toContain("Calories");
+  });
+
+  it("shows Kilojoules when energyUnits is 'kilojoules'", async () => {
+    wrapper.setProps({ energyUnits: "kilojoules" });
+    await flushPromises();
+    expect(wrapper.text()).toContain("Kilojoules");
+  });
 });
